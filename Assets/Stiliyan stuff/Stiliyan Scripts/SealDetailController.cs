6531ce97431dffa3a45f542244a0fe14f6c8b005
stenli1902 <halls2@yahoo.com>
2025-03-31 06:51:23 +0300

    all traits updated

--- Assets/Stiliyan stuff/Stiliyan Scripts/SealDetailController.cs
+++ Assets/Stiliyan stuff/Stiliyan Scripts/SealDetailController.cs
@@ -12,6 +12,9 @@ public class SealDetailController : MonoBehaviour
     public Slider healthSlider;
     public Slider moodSlider;
     public Slider hungerSlider;
+    public Text moodTrait;
+    public Text hungerTrait;
+    public Text healthTrait;
 
 
     void Start()
@@ -24,6 +27,44 @@ public class SealDetailController : MonoBehaviour
         healthSlider.value = seal.health;
         moodSlider.value = seal.mood;
         hungerSlider.value = seal.hunger;
+
+        if (seal.moodTrait == 1)
+        {
+            moodTrait.text = "Lazy";
+        }
+        else if (seal.moodTrait == 2)
+        {
+            moodTrait.text = "Normal";
+        }
+        else if (seal.moodTrait == 3)
+        {
+            moodTrait.text = "Energetic";
+        }
+
+        if (seal.hungerTrait == 1)
+        {
+            hungerTrait.text = "Fussy";
+        }
+        else if (seal.hungerTrait == 2)
+        {
+            hungerTrait.text = "Normal";
+        }
+        else if (seal.hungerTrait == 3)
+        {
+            hungerTrait.text = "Foodie";
+        }
+
+        if (seal.healthTrait == 1)
+        {
+            healthTrait.text = "Brittle";
+        }
+        else if (seal.healthTrait == 2)
+        {
+            healthTrait.text = "Normal";
+        }else if (seal.healthTrait == 3)
+        {
+            healthTrait.text="Robust";
+        }
 
         backButton.onClick.AddListener(() => SceneManager.LoadScene("HabitatScene"));
     }
