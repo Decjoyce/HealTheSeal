80f4db1441d6329315b0df7a4f259b07ef2df943
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2025-04-11 14:12:40 +0100

    stuuff

---
+++ Assets/Game/Scripts/Seal/SO_SealGraphics.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SO_SealGraphics : ScriptableObject
+{
+    public Sprite seal_small_normal;
+    public Sprite seal_small_sad;
+    public Sprite seal_medium_normal;
+    public Sprite seal_medium_sad;
+    public Sprite seal_big_normal;
+    public Sprite seal_big_sad;
+}
