895cd301c12677eac56f0eb196c6053955e5a94f
stenli1902 <halls2@yahoo.com>
2025-04-20 23:23:25 +0300

    Scrollable scene, seal teleportation
    Instead of working with the UI I decided to work with game objects since the seal is a game object and parenting it to the ui doesnt work. So I decided to make the camera moveable. Using the camera the player can "scroll" the habitat scene.
    The seals now teleport to a vector 2 ( which has to be adjusted by going in the seal prefab) depending on health. I dont have the scene images so I didnt add any colliders to separate the different zones but this is very easy to do so have fun with it.

--- Assets/Game/Scripts/Seal/SealBehaviour.cs
+++ Assets/Game/Scripts/Seal/SealBehaviour.cs
@@ -7,10 +7,20 @@ public class SealBehaviour : MonoBehaviour
     public Seal sealData;
     TextMeshPro text_name; //Prototype
 
+    public Vector2 lowHPPosition;    // for HP: 0–30
+    public Vector2 midHPPosition;    // for HP: 31–60
+    public Vector2 highHPPosition;   // for HP: 61–100
+
+    private int previousHealth;
+
     private void Start()
     {
         text_name = GetComponentInChildren<TextMeshPro>(); //Prototype
         text_name.text = sealData.seal_name; //Prototype
+
+        previousHealth = (int)sealData.health;
+        UpdateSealPosition();
+
     }
 
     public void SetSealData(Seal seal)
@@ -34,9 +44,26 @@ public class SealBehaviour : MonoBehaviour
             SceneManager.LoadScene("SealDetailScene");
         }
     }
+    void UpdateSealPosition()
+    {
+        if (sealData.health <= 30)
+            transform.position = lowHPPosition;
+        else if (sealData.health <= 60)
+            transform.position = midHPPosition;
+        else
+            transform.position = highHPPosition;
+    }
 
     void Update()
     {
+
+        if (previousHealth != (int)sealData.health)
+        {
+            previousHealth = (int)sealData.health;
+            UpdateSealPosition();
+        }
+
+
         // Simple random movement
         transform.position += new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0) * Time.deltaTime;
         sealData.position = transform.position;
